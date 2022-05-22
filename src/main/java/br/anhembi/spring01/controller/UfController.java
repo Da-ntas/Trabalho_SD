e885a3696193ea58c9b44a7eef7656184dd4fe56
Da-ntas <hugodfl2002@gmail.com>
2022-05-21 22:27:24 -0300

    Posts Gerais

--- src/main/java/br/anhembi/spring01/controller/UfController.java
+++ src/main/java/br/anhembi/spring01/controller/UfController.java
@@ -3,8 +3,11 @@ package br.anhembi.spring01.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +26,14 @@ public class UfController {
     public ResponseEntity <List<Uf>> findAll(){
         List<Uf> listuser = (List<Uf>) repo.findAll();
         return ResponseEntity.ok(listuser);
+    }
+
+    
+    @PostMapping
+    public ResponseEntity<Uf> insertUfs(@RequestBody Uf uf){
+
+        Uf newuf = repo.save(uf);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newuf);
     }
 
 }
--- src/main/java/br/anhembi/spring01/controller/UnidadesController.java
+++ src/main/java/br/anhembi/spring01/controller/UnidadesController.java
@@ -3,8 +3,11 @@ package br.anhembi.spring01.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,4 +28,12 @@ public class UnidadesController {
         return ResponseEntity.ok(listuser);
     }
 
+    @PostMapping
+    public ResponseEntity<Unidades> insertUser(@RequestBody Unidades unidade){
+
+        Unidades newUnidade = repo.save(unidade);
+        return ResponseEntity.status(HttpStatus.CREATED).body(newUnidade);
+    }
+
 }
+
